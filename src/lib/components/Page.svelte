08ff61334bf776e5cb24194dbd60bf5228176d28
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2022-04-01 14:16:23 -0300

    Add Error page

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -17,15 +17,17 @@
 </svelte:head>
 
 <div class="container">
-  <h3
-    class="mb-3 p-1 border-b-1 border-default-light flex flex-row justify-start items-center gap-3"
-  >
-    {#if back}
-      <button class="btn btn-fit btn-link primary" on:click={handleBackClick}>
-        <ArrowLeftIcon class="icon icon-fill" />
-      </button>
-    {/if}
-    {title}
-  </h3>
+  <slot name="title" back={handleBackClick} {title}>
+    <h3
+      class="mb-3 p-1 border-b-1 border-default-light flex flex-row justify-start items-center gap-3"
+    >
+      {#if back}
+        <button class="btn btn-fit btn-link primary" on:click={handleBackClick}>
+          <ArrowLeftIcon class="icon icon-fill" />
+        </button>
+      {/if}
+      {title}
+    </h3>
+  </slot>
   <slot />
 </div>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,37 @@
+<script context="module" lang="ts">
+  import type { ErrorLoad } from '@sveltejs/kit'
+
+  export const load: ErrorLoad = ({ error, status }) => {
+    return {
+      props: {
+        message: error.message,
+        status,
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  import Card from '$lib/components/Card.svelte'
+  import Page from '$lib/components/Page.svelte'
+  import ExclamationIcon from '$lib/icons/exclamation.svg?component'
+
+  export let message: string
+  export let status: number
+</script>
+
+<div class="w-full md:max-w-md">
+  <Page title={`Error ${status}`}>
+    <div slot="title" class="w-full flex items-center justify-center" let:title>
+      <Card>
+        <div class="w-full flex flex-col gap-6 items-center overflow-x-auto">
+          <div class="icon-3xl">
+            <ExclamationIcon class="icon-fill danger" />
+          </div>
+          <h2 class="text-danger-base">{title}</h2>
+          <h5 class="text-danger-base">{message}</h5>
+        </div>
+      </Card>
+    </div>
+  </Page>
+</div>
