47c6afc9504c2596ede77f93bf62a928e5f04ad1
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2022-03-31 22:49:31 -0300

    Register SW

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import svg from '@poppanator/sveltekit-svg'
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +18,7 @@ const config = {
     },
 
     vite: {
-      plugins: [svg()],
+      plugins: [svg(), VitePWA({ manifest: false })],
     },
   },
 }
