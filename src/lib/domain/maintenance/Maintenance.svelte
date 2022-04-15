8f0ddac8945fa86f132a051b406e65acb70c13a1
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2022-04-15 19:33:11 -0300

    Replace deprecated stores by services

--- src/lib/domain/maintenance/Maintenance.svelte
+++ src/lib/domain/maintenance/Maintenance.svelte
@@ -2,9 +2,11 @@
   import Card from '$lib/components/Card.svelte'
   import Page from '$lib/components/Page.svelte'
   import { useTranslate } from '$lib/config'
-  import { loadCustomers } from '$lib/domain/customer/customer.store'
-  import type { StorageKey } from '$lib/domain/maintenance/Maintenance'
-  import { storageKeyStore } from '$lib/domain/maintenance/maintenance.store'
+  import {
+    computeStorageKeys,
+    getStorageKeys,
+    type StorageKey,
+  } from '$lib/domain/maintenance/Maintenance'
   import StorageKeyDetail from '$lib/domain/maintenance/StorageKeyDetail.svelte'
   import StorageKeyTable from '$lib/domain/maintenance/StorageKeyTable.svelte'
 
@@ -16,14 +18,12 @@
 
   let viewKey: StorageKey | undefined = undefined
 
-  $: {
-    loadCustomers()
-  }
+  $: keys = computeStorageKeys(getStorageKeys())
 </script>
 
 <Page title={$t('maintenanceView')}>
   <Card>
-    <StorageKeyTable items={$storageKeyStore} bind:view={viewKey} />
+    <StorageKeyTable items={keys} bind:view={viewKey} />
   </Card>
 </Page>
 <StorageKeyDetail bind:item={viewKey} />
